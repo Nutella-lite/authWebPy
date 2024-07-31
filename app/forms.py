b866defb58af69d80f1051f1cdeaec101d8421a9
Nutella-lite <natalysny2012@gmail.com>
2024-07-31 18:03:50 +0300

    update project

--- app/forms.py
+++ app/forms.py
@@ -5,11 +5,11 @@ from app.models import User
 from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=35)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    username = StringField('Как к Вам обращаться?', validators=[DataRequired(), Length(min=2, max=35)])
+    email = StringField('Ваш email', validators=[DataRequired(), Email()])
+    password = PasswordField('Придумайте пароль', validators=[DataRequired()])
+    confirm_password = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Подтердить')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -26,14 +26,14 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember = BooleanField('Запомнить меня')
-    submit = SubmitField('Login')
+    submit = SubmitField('Войти')
 
 class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password')
-    confirm_password = PasswordField('Confirm Password', validators=[EqualTo('password')])
-    submit = SubmitField('Update')
+    username = StringField('Как к Вам обращаться?', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Ваш email', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль')
+    confirm_password = PasswordField('Повторите пароль', validators=[EqualTo('password')])
+    submit = SubmitField('Сохранить')
 
     def validate_username(self, username):
         if username.data != current_user.username:
